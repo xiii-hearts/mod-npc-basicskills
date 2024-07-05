4043bae0d852ef7fb7a8685762042a0876506886
xiiihearts <141794262+xiii-hearts@users.noreply.github.com>
2024-07-05 10:05:08 +0800

    Create npcbasicskills.sql

---
+++ sql/world/npcbasicskills.sql
@@ -0,0 +1,33 @@
+-- --------------------------------------------------------------------------------------
+--	BASIC SUBCLASS SKILLS TRAINER - 600002
+-- --------------------------------------------------------------------------------------
+SET
+@Entry 		:= 600002,
+@Model 		:= 26571, -- The Black Knight
+@Name 		:= "The Half-Pint Goliath",
+@Title 		:= "Basic Subclass Skills Trainer",
+@Icon 		:= "Speak",
+@GossipMenu := 0,
+@MinLevel 	:= 80,
+@MaxLevel 	:= 80,
+@Faction 	:= 35,
+@NPCFlag 	:= 1,
+@Scale		:= 0.5,
+@Rank		:= 0,
+@Type 		:= 7,
+@TypeFlags 	:= 0,
+@FlagsExtra := 2,
+@AIName		:= "SmartAI",
+@Script 	:= "BasicSkills_NPC";
+
+-- NPC
+DELETE FROM `creature_template` WHERE `entry` = @Entry;
+INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `AiName`, `ScriptName`) VALUES
+(@Entry, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, 1, 1.14286, 1, @Rank, 1, 2, @Type, @TypeFlags, 1, @FlagsExtra, @AIName, @Script);
+
+DELETE FROM `creature_template_model` WHERE `CreatureID` = @Entry;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`) VALUES
+(@Entry, 0, @Model, @Scale, 1);
+
+DELETE FROM `npc_text` WHERE `ID` = @Entry;
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry, 'Greetings $N. I can teach you about basic skills to subclassing!!!');
